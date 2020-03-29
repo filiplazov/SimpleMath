6d7465c329c7ed9562d78301be71426be0c6533f
Filip Lazov <filiplazov@gmail.com>
2020-03-29 21:19:11 +0200

    Moved the font modifier out of the `CommandInputButton`

--- SimpleMath/views/CommandInputButton.swift
+++ SimpleMath/views/CommandInputButton.swift
@@ -12,9 +12,8 @@ struct CommandInputButton: View {
   
   var body: some View {
     Button(action: { withAnimation { self.action() } }, label: { Image(systemName: symbol.rawValue) })
-      .font(Font.system(size: 60, weight: .heavy, design: .monospaced))
       .foregroundColor(color.opacity(isEnabled ? 1.0 : 0.2))
       .disabled(!isEnabled)
-      .padding([.leading, .trailing], 30)
+      .padding()
   }
 }
