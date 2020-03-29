a9d64b0140cb9d71c17ed8a1b9d404a79cad552e
Filip Lazov <filiplazov@gmail.com>
2020-03-29 21:18:35 +0200

    `NumbersInputView` now has a `spacing` parameter which uses to apply spacing for each of the number rows

--- SimpleMath/views/NumbersInputView.swift
+++ SimpleMath/views/NumbersInputView.swift
@@ -5,32 +5,31 @@
 import SwiftUI
 
 struct NumbersInputView: View {
+  let spacing: CGFloat
   let action: (Int) -> Void
   
   var body: some View {
     VStack {
-      HStack(spacing: 100) {
+      HStack(spacing: self.spacing) {
         ForEach(1...3, id: \.self) { index in
           Button(index.description) { self.action(index) }
         }
       }
-      HStack(spacing: 100) {
+      HStack(spacing: self.spacing) {
         ForEach(4...6, id: \.self) { index in
           Button(index.description) { self.action(index) }
         }
       }
-      HStack(spacing: 100) {
+      HStack(spacing: self.spacing) {
         ForEach(7...9, id: \.self) { index in
           Button(index.description) { self.action(index) }
         }
       }
-      HStack(spacing: 100) {
+      HStack(spacing: self.spacing) {
         Button("0") { self.action(0) }
       }
       
     }
     .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-    .font(Font.system(size: 90, weight: .heavy, design: .monospaced))
-    .padding([.leading, .trailing], 30)
   }
 }
