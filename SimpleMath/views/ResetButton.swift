2a7f2959947e979422d3edf5552a653c5a05de0b
Filip Lazov <filiplazov@gmail.com>
2020-03-29 21:23:54 +0200

    `ResetButton` now uses horizontal size class to dynamically set the font size

--- SimpleMath/views/ResetButton.swift
+++ SimpleMath/views/ResetButton.swift
@@ -5,14 +5,16 @@
 import SwiftUI
 
 struct ResetButton: View {
+  @Environment(\.horizontalSizeClass) private var hSizeClass
   let action: () -> Void
   
   var body: some View {
     Button(action: self.action) {
       Image(systemName: Symbol.reset.rawValue)
-        .padding([.trailing, .top], 40)
-        .font(Font.system(size: 80, weight: .bold, design: .monospaced))
+        .font(Font.system(size: self.hSizeClass.isRegular ? 80 : 40, weight: .bold, design: .monospaced))
         .foregroundColor(Color(#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)))
     }
+    .padding(.trailing, 20)
+    .padding(.top, 34)
   }
 }
