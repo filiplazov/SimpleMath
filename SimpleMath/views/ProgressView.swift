ac51aab60dc327287d9cbef75f5640fa64466ce1
Filip Lazov <filiplazov@gmail.com>
2020-03-29 21:27:31 +0200

    `ProgressView` uses a new parameter `width` to dynamically scale for any width and now correctly encompases the rings + their strokes inside the view "bounds"

--- SimpleMath/views/ProgressView.swift
+++ SimpleMath/views/ProgressView.swift
@@ -5,25 +5,35 @@
 import SwiftUI
 
 struct ProgressView: View {
-  private let width: CGFloat = 60
+  let width: CGFloat
+  let strokeWidth: CGFloat
   let progress: Double
+  let circleSize: CGFloat
+  
+  init(width: CGFloat, progress: Double) {
+    self.width = width
+    self.progress = progress
+    let strokeScale: CGFloat = 0.13
+    circleSize = width / ( 1 + strokeScale)
+    strokeWidth = circleSize * strokeScale
+  }
   
   var body: some View {
     ZStack {
       Circle()
-        .stroke(lineWidth: 10)
-        .frame(width: width, height: width)
+        .stroke(lineWidth: strokeWidth)
+        .frame(width: circleSize, height: circleSize)
         .foregroundColor(Color(#colorLiteral(red: 0.3870187126, green: 0.08371740527, blue: 0.3813446013, alpha: 1)))
       Circle()
         .trim(from: CGFloat(1 - progress), to: 1)
-        .stroke(lineWidth: 10)
+        .stroke(lineWidth: strokeWidth)
         .rotationEffect(.degrees(90))
         .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0))
-        .frame(width: width, height: width)
+        .frame(width: circleSize, height: circleSize)
         .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)).opacity(0.7))
     }
+      
+    .frame(width: width, height: width)
     .opacity(progress > 0 ? 1.0 : 0.0)
-    .padding(.top, 50)
-    .padding(.leading, 22)
   }
 }
