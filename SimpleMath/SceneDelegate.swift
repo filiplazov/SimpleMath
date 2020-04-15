b65a6101f7f6dd8a940630df78612ef457d3f545
Filip Lazov <filiplazov@gmail.com>
2020-04-16 00:06:41 +0200

    Updated SceneDelegate to use App

--- SimpleMath/SceneDelegate.swift
+++ SimpleMath/SceneDelegate.swift
@@ -8,23 +8,18 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   
   var window: UIWindow?
-  
+  var app: App?
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-    
-    // Create the SwiftUI view that provides the window contents.
-    let contentView = ContentView(viewModel: SimpleMathViewModel())
-    
-    // Use a UIHostingController as window root view controller.
     if let windowScene = scene as? UIWindowScene {
+      
       let window = UIWindow(windowScene: windowScene)
-      let controller = HostingController(rootView: contentView)
-      window.rootViewController = controller
       self.window = window
-      window.makeKeyAndVisible()
+      app = App(window: window)
+      app?.startApp()
     }
   }
   
@@ -58,4 +53,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   
   
 }
-
