de456a70f8734ad72ff450412b8d6eb4d8680ad2
Filip Lazov <filiplazov@gmail.com>
2020-04-16 00:16:26 +0200

    Added static constant extensions to CGSize and CGFloat which initialize form UIScreen.main.bounds for convinience

---
+++ SimpleMath/UI/Helpers/ScreenExtensions.swift
@@ -0,0 +1,14 @@
+//  SimpleMath
+//  Copyright (c) Filip Lazov 2020
+//  MIT license - see LICENSE file for more info
+
+import UIKit
+
+extension CGSize {
+  static let screen = UIScreen.main.bounds.size
+}
+
+extension CGFloat {
+  static let screenWidth = CGSize.screen.width
+  static let screenHeight = CGSize.screen.height
+}
