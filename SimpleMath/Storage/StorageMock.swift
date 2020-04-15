e569f2c02b5dfa9f820a0e01bf8a9783c1a6f772
Filip Lazov <filiplazov@gmail.com>
2020-04-16 00:00:32 +0200

    Added UserDefaultsStorage which implements Storage and performs loading and storing to user defaults in JSON encoded data format

---
+++ SimpleMath/Storage/StorageMock.swift
@@ -0,0 +1,29 @@
+//  SimpleMath
+//  Copyright (c) Filip Lazov 2020
+//  MIT license - see LICENSE file for more info
+
+final class StorageMock: Storage {
+  var settingsBundle: SettingsBundle
+  var onboardingBundle: OnboardingBundle
+  
+  init(settingsBundle: SettingsBundle = .default, onboardingBundle: OnboardingBundle = .default) {
+    self.settingsBundle = settingsBundle
+    self.onboardingBundle = onboardingBundle
+  }
+  
+  func store(settingsBundle: SettingsBundle) {
+    self.settingsBundle = settingsBundle
+  }
+  
+  func loadSettingsBundle() -> SettingsBundle {
+    settingsBundle
+  }
+  
+  func store(onboardingBundle: OnboardingBundle) {
+    self.onboardingBundle = onboardingBundle
+  }
+  
+  func loadOnboardingBundle() -> OnboardingBundle {
+    onboardingBundle
+  }
+}
---
+++ SimpleMath/Storage/UserDefaultsStorage.swift
@@ -0,0 +1,82 @@
+//  SimpleMath
+//  Copyright (c) Filip Lazov 2020
+//  MIT license - see LICENSE file for more info
+
+import Foundation
+
+final class UserDefaultsStorage: Storage {
+  private var storedDataCache: StoredData?
+  let modelVersion: String
+  let key: String
+  
+  init(withKey: String, modelVersion: String) {
+    key = withKey
+    self.modelVersion = modelVersion
+  }
+  
+  func store(settingsBundle: SettingsBundle) {
+    var data = loadStoredData()
+    data.settingsBundle = settingsBundle
+    save(storedData: data)
+    
+  }
+  
+  func loadSettingsBundle() -> SettingsBundle {
+    loadStoredData().settingsBundle
+  }
+  
+  func store(onboardingBundle: OnboardingBundle) {
+    var data = loadStoredData()
+    data.onboardingBundle = onboardingBundle
+    save(storedData: data)
+  }
+  
+  func loadOnboardingBundle() -> OnboardingBundle {
+    loadStoredData().onboardingBundle
+  }
+  
+  private func loadStoredData() -> StoredData {
+    if let cache = storedDataCache {
+      return cache
+    } else {
+      var storedData: StoredData
+      do {
+        if let data = UserDefaults.standard.value(forKey: key) as? Data {
+          storedData = try JSONDecoder().decode(StoredData.self, from: data)
+        } else {
+          print("No data found for key \(key), generating default values")
+          storedData = .useDefault
+          storedData.modelVersion = modelVersion
+        }
+      } catch {
+        print("error reading stored data \(error), generating default values")
+        storedData = .useDefault
+        storedData.modelVersion = modelVersion
+      }
+      storedDataCache = storedData
+      return storedData
+    }
+  }
+  
+  private func save(storedData: StoredData) {
+    storedDataCache = storedData
+    do {
+      try UserDefaults.standard.set(JSONEncoder().encode(storedData), forKey: key)
+    } catch {
+      print("failed saving data :", error)
+    }
+  }
+}
+
+// the model version is stored if migrations are needed in the future
+private struct StoredData: Codable {
+  var modelVersion: String
+  var settingsBundle: SettingsBundle
+  var onboardingBundle: OnboardingBundle
+}
+
+extension StoredData {
+  static var useDefault: StoredData {
+    StoredData(modelVersion: "", settingsBundle: .default, onboardingBundle: .default)
+  }
+}
