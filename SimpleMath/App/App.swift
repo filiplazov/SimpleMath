b342e29d423dbe8735e6b32a3e2daeb2c6618b50
Filip Lazov <filiplazov@gmail.com>
2020-04-16 00:06:24 +0200

    Added App, entry point created forom the SceneDelegate which sets up dependency injection as well as some helpers implementation to manage the keyboard dismissal

---
+++ SimpleMath/App/App.swift
@@ -0,0 +1,104 @@
+//  SimpleMath
+//  Copyright (c) Filip Lazov 2020
+//  MIT license - see LICENSE file for more info
+
+import UIKit
+import Combine
+
+final class App {
+  private var recognizerDelegate = RecognizerDelegate()
+  private var keyboardObserver = KeyboardObserver()
+  private var gesture: AnyGestureRecognizer?
+  private let storage: Storage
+  private var settings: Settings
+  weak var window : UIWindow?
+  
+  init(window: UIWindow) {
+    self.window = window
+    storage = UserDefaultsStorage(withKey: App.identifier, modelVersion: App.version)
+    settings = StoredSettings(withStorage: storage)
+  }
+  
+  func startApp() {
+    
+    let contentView = ContentView()
+      .environmentObject(SimpleMathViewModel(settings: settings))
+      .environmentObject(SettingsViewModel(settings: settings))
+      .environmentObject(Onboarding(withStorage: storage))
+    
+    let controller = HostingController(rootView: contentView)
+    window?.rootViewController = controller
+    
+    gesture = AnyGestureRecognizer(target: window, action:#selector(UIView.endEditing))
+    gesture?.requiresExclusiveTouchType = false
+    gesture?.cancelsTouchesInView = false
+    gesture?.delegate = recognizerDelegate
+    
+    keyboardObserver.onShow = { [weak self] in
+      guard let gesture = self?.gesture else { return }
+      self?.window?.addGestureRecognizer(gesture)
+    }
+    
+    keyboardObserver.onHide = { [weak self] in
+      guard let gesture = self?.gesture else { return }
+      self?.window?.removeGestureRecognizer(gesture)
+    }
+    window?.makeKeyAndVisible()
+  }
+}
+
+extension App {
+  static var version: String {
+    Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+  }
+  
+  static var identifier: String {
+    Bundle.main.bundleIdentifier ?? ""
+  }
+}
+
+private class KeyboardObserver {
+  private var subcriptions = Set<AnyCancellable>()
+  var onShow: (() -> Void)?
+  var onHide: (() -> Void)?
+  
+  init() {
+    NotificationCenter.default
+      .publisher(for: UIResponder.keyboardDidShowNotification)
+      .sink(receiveValue: { [weak self] _ in
+        self?.onShow?()
+      })
+      .store(in: &subcriptions)
+    
+    NotificationCenter.default
+      .publisher(for: UIResponder.keyboardDidHideNotification)
+      .sink(receiveValue: { [weak self] _ in
+        self?.onHide?()
+      })
+      .store(in: &subcriptions)
+    
+  }
+}
+
+class AnyGestureRecognizer: UIGestureRecognizer {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        state = .began
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+       state = .ended
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+        state = .cancelled
+    }
+}
+
+private final class RecognizerDelegate: NSObject, UIGestureRecognizerDelegate {
+  func gestureRecognizer(
+    _ gestureRecognizer: UIGestureRecognizer,
+    shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+  ) -> Bool {
+      return true
+  }
+}
