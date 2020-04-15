1896332d8c7f52a22ccda5dcec732e1974e46887
Filip Lazov <filiplazov@gmail.com>
2020-04-15 23:50:45 +0200

    Added EquationType with cases for each of the 4 types: addition, subtraction, multiplication, division

---
+++ SimpleMath/App/Equations/EquationType.swift
@@ -0,0 +1,10 @@
+//  SimpleMath
+//  Copyright (c) Filip Lazov 2020
+//  MIT license - see LICENSE file for more info
+
+enum EquationType: String, Codable, CaseIterable {
+  case addition
+  case subtraction
+  case multiplication
+  case division
+}
