2082302e8b8002b50797487bd74a7d9f06141ff2
Filip Lazov <filiplazov@gmail.com>
2020-04-15 23:57:05 +0200

    Sound is now seperated in its own file and not nested in former AudioPlayer

---
+++ SimpleMath/Audio Engine/Sound.swift
@@ -0,0 +1,9 @@
+//  SimpleMath
+//  Copyright (c) Filip Lazov 2020
+//  MIT license - see LICENSE file for more info
+
+enum Sound: String, CaseIterable {
+  case greatSuccess = "great-success"
+  case success
+  case failure
+}
