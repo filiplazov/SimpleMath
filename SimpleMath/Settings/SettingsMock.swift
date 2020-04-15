3fdf133fc286390f4bef0d0fa275f01bf6426e9c
Filip Lazov <filiplazov@gmail.com>
2020-04-16 00:01:59 +0200

    Added StoredSettings which imlpements Settings and uses Storage to store and load settings to / from persistance

---
+++ SimpleMath/Settings/SettingsMock.swift
@@ -0,0 +1,17 @@
+//  SimpleMath
+//  Copyright (c) Filip Lazov 2020
+//  MIT license - see LICENSE file for more info
+
+import Combine
+
+final class SettingsMock: Settings {
+  var updatedBundle: SettingsBundle?
+  var currentSettings: AnyPublisher<SettingsBundle, Never> = Empty<SettingsBundle, Never>()
+    .eraseToAnyPublisher()
+  
+  func updateSettings(bundle: SettingsBundle) {
+    self.updatedBundle = bundle
+  }
+  
+  
+}
