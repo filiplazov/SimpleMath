d26d4784e8e3099b7c504bd703e657fa7ac97319
Filip Lazov <filiplazov@gmail.com>
2020-04-16 00:11:56 +0200

    HostingViewController is now a proper generic subclass of UIHostingViewController

---
+++ SimpleMath/UI/Helpers/HostingViewController.swift
@@ -0,0 +1,11 @@
+//  SimpleMath
+//  Copyright (c) Filip Lazov 2020
+//  MIT license - see LICENSE file for more info
+
+import SwiftUI
+
+class HostingController<Content> : UIHostingController<Content> where Content: View {
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+}
