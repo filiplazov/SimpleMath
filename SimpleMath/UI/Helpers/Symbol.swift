4c131853fea5042142e71f5fe0da8e444e3678ce
Filip Lazov <filiplazov@gmail.com>
2020-04-16 00:12:19 +0200

    Added all SF symbol names used in the app to Symbol

---
+++ SimpleMath/UI/Helpers/Symbol.swift
@@ -0,0 +1,17 @@
+//  SimpleMath
+//  Copyright (c) Filip Lazov 2020
+//  MIT license - see LICENSE file for more info
+
+enum Symbol: String {
+  case erase = "arrow.left.circle"
+  case star = "star.fill"
+  case reset = "arrow.counterclockwise.circle"
+  case plus = "plus"
+  case minus = "minus"
+  case leftArrow = "arrow.left"
+  case rightArrow = "arrow.right"
+  case checkmarkFilled = "checkmark.circle.fill"
+  case checkmark = "checkmark.circle"
+  case soundsEnabled = "speaker.3"
+  case soundsDisabled = "speaker.slash"
+}
