4e6e248c67520b93d95b661940a2f23309a7eea9
Filip Lazov <filiplazov@gmail.com>
2020-04-24 18:32:18 +0200

    Workaround to avoid retaining self when using assign and storing subscriptions in self

---
+++ SimpleMath/App/Helpers/Assign+WeakCapture.swift
@@ -0,0 +1,16 @@
+//  SimpleMath
+//  Copyright (c) Filip Lazov 2020
+//  MIT license - see LICENSE file for more info
+
+import Combine
+
+// This is a workaround to avoid strong self capturing (and causing retain cycle) when using assign on self and storing subcription in self
+// https://forums.swift.org/t/does-assign-to-produce-memory-leaks/29546
+
+extension Publisher where Failure == Never {
+    func assign<Root: AnyObject>(to keyPath: ReferenceWritableKeyPath<Root, Output>, on root: Root) -> AnyCancellable {
+       sink { [weak root] in
+            root?[keyPath: keyPath] = $0
+        }
+    }
+}
